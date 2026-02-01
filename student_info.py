2e3de2ed7ef03fa47fccb6c5ef9c2dc5d2c0cbbd
deriqsanders <deriqsanders@gmail.com>
2026-01-31 20:11:56 -0500

    Add hobby field to student profile

--- student_info.py
+++ student_info.py
@@ -3,6 +3,7 @@ major = "Information Technology"
 graduation_year = 2026
 email = "sanders_deriq@students.columbusstate.edu"
 hometown= "Phenix City, AL"
+hobby= "Drawing"
 
 print("==========================================")
 print("       STUDENT INFORMATION CARD")
@@ -12,4 +13,5 @@ print(f"Major: {major}")
 print(f"Graduation Year: {graduation_year}")
 print(f"Email: {email}")
 print(f"Hometown: {hometown}")
+print(f"Hobby: {hobby}")
 print("==========================================")
